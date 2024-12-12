2a5ce95f168e3cdbcf180840947f389f3bdc1d97
mac <wt@lycam.tv>
2024-12-11 17:55:23 -0700

    no message

--- src/shred_collector/shred_tracker.zig
+++ src/shred_collector/shred_tracker.zig
@@ -14,7 +14,7 @@ const MAX_SHREDS_PER_SLOT: usize = sig.ledger.shred.MAX_SHREDS_PER_SLOT;
 
 const MIN_SLOT_AGE_TO_REPORT_AS_MISSING: u64 = 200;
 
-const MAX_RETRY_COUNT = 100;
+const MAX_RETRY_COUNT = 10;
 
 pub const Range = struct {
     start: usize,
@@ -150,29 +150,29 @@ pub const BasicShredTracker = struct {
             slot_report.slot = slot;
             try monitored_slot.identifyMissing(&slot_report.missing_shreds);
             // if (slot_report.missing_shreds.items.len > 0) {
-            if (slot_report.missing_shreds.items.len > 0) { //and self.isMaxTryRepaireCount(slot) == false) {
+            if (slot_report.missing_shreds.items.len > 0 and self.isMaxTryRepaireCount(slot) == false) {
                 found_an_incomplete_slot = true;
                 // self.logger.debug().logf(
                 //     "slot_report.missing_shreds.items.len {}",
                 //     .{slot_report.missing_shreds.items.len},
                 // );
-                // if (self.tryCount.get(slot)) |count| {
-                //     // var c = count + 1;
-                //     self.tryCount.put(slot, count + 1) catch |err| {
-                //         self.logger.err().logf("Failed to update tryCount: {}", .{err});
-                //     };
-                // } else {
-                //     self.tryCount.put(slot, 1) catch |err| {
-                //         self.logger.err().logf("Failed to update tryCount: {}", .{err});
-                //     };
-                // }
+                if (self.tryCount.get(slot)) |count| {
+                    // var c = count + 1;
+                    self.tryCount.put(slot, count + 1) catch |err| {
+                        self.logger.err().logf("Failed to update tryCount: {}", .{err});
+                    };
+                } else {
+                    self.tryCount.put(slot, 1) catch |err| {
+                        self.logger.err().logf("Failed to update tryCount: {}", .{err});
+                    };
+                }
             } else {
-                // if (slot_report.missing_shreds.items.len > 0) {
-                //     self.logger.debug().logf(
-                //         "slot_report.missing_shreds.items.len {}",
-                //         .{slot_report.missing_shreds.items.len},
-                //     );
-                // }
+                if (slot_report.missing_shreds.items.len > 0) {
+                    self.logger.debug().logf(
+                        "slot_report.missing_shreds.items.len {}",
+                        .{slot_report.missing_shreds.items.len},
+                    );
+                }
                 slot_reports.drop(1);
             }
             if (!found_an_incomplete_slot) {
